9960e809382df83ad76151f836ad6431dd47334c
Hamin <haminjp@naver.com>
2021-02-28 14:11:17 +0900

    feat: useReducer

--- use-reducer/src/App.js
+++ use-reducer/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from 'react';
+import Todo from './Todo.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
 }
 
-export default App;
+const reducer = (todos, action) => {
+  switch(action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if(todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id);
+    default:
+      return todos;
+  }
+};
+
+const newTodo = (name) => {
+  return { id: Date.now(), name: name, complete: false }
+};
+
+export default function App() {
+  const [name, setName] = useState('');
+  const [todos, dispatch] = useReducer(reducer, []);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName('');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
+    </>
+  );
+}
